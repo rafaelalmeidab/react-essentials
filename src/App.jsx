8ef8988e1a6a115d1a5f14d902e954b95cf564d0
Rafael Almeida <rafaelalmeidab@hotmail.com>
2024-09-10 10:32:15 -0300

    feature: using fragments

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -47,7 +47,7 @@ function App() {
           }
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
